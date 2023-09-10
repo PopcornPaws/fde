37341ce6ca97e11fafbaeb66bfca45c302d83a42
PopcornPaws <lipschitz77@gmail.com>
2023-09-10 10:31:43 +0200

    Wip trait system

--- examples/protocol.rs
+++ examples/protocol.rs
@@ -1,10 +1,68 @@
+use ark_ec::pairing::Pairing;
+
+// encryption engines
+pub struct Generic;
+pub struct ElGamal;
+pub struct Paillier;
+
+impl EncryptionEngine for Generic {
+    type Output = Vec<u8>;
+    fn encrypt(&self, data: &[u8]) -> Self::Output {
+        Vec::new()
+    }
+}
+
+pub trait EncryptionEngine {
+    type Output;
+    fn encrypt(&self, data: &[u8]) -> Self::Output;
+}
+
+// backend types
+// pub struct Snark;
+// pub struct Ipa;
+// pub struct Kzg;
+
+pub trait Backend {
+    type Curve: Pairing;
+    type EncryptionEngine: EncryptionEngine; // TODO bound by curve somehow?
+    type Commitment; // TODO bound by curve somehow?
+}
+
 pub trait Server {
-    type Curve; // pairing friendly curve - bound by traits
-    type EncrypionEngine; // Type of encryption - bound by traits
-    fn data(&self) -> &[u8];
-    fn commit<C>(&self) -> C; // commitment (generic - should be bounded by Self::...)
-    fn encrypt<E>(&self) -> E; // encryption (generic - should be bounded by Self::...)
+    type Fdx: Backend;
+    fn commit(&self, data: &[u8]) -> Commitment;
+    fn encrypt(&self, data: &[u8]) -> <<Self::Fdx as Backend>::EncryptionEngine as EncryptionEngine>::Output;
     fn adapt(&self, pre_sig: PreSignature) -> Signature;
 }
+
+pub trait Client {
+    type DataSource: Server;
+    fn check(
+        commitment: Commitment,
+        encryption: <<<Self::DataSource as Server>::Backend as Backend>::EncryptionEngine as EncryptionEngine>::Output,
+    ) -> bool;
+    fn pre_sign(&self, tx: Transaction, pk_s: PublicKey) -> PreSignature;
+    fn extract(&self, signature: Signature, pre_sig: PreSignature, pk_s: PublicKey) -> SecretKey;
+    fn decrypt(&self, sk_s: SecretKey) -> Vec<u8>;
+}
+
 fn main() {
+    /* SERVER
+     *
+     * new(data, sk_s)
+     * commit(data)
+     * encrypt(data, sk_s)
+     * adapt(pre_signature, sk_s)
+     *
+     */
+
+    /* CLIENT
+     *
+     * new(secret_key) -> Self
+     * check(commitment, encryption) -> bool
+     * pre_sign(sk_c, transaction, pk_s) -> PreSignature
+     * extract(signature, pre_signature, pk_s) -> SecretKey
+     * decrypt(sk_s, encryption) -> Vec<u8>
+     *
+     */
 }
