29e21058338d91c54b158cdae06b3c5669e45cdd
PopcornPaws <lipschitz77@gmail.com>
2023-09-11 18:37:36 +0200

    Wip

--- src/adaptor_sig.rs
+++ src/adaptor_sig.rs
@@ -1,82 +1,70 @@
-use ark_crypto_primitives::signature::SignatureScheme;
+use ark_crypto_primitives::signature::{schnorr::Schnorr, SignatureScheme};
 use ark_crypto_primitives::Error;
 use ark_ec::{AffineRepr, CurveGroup};
 use ark_ff::{UniformRand, Zero};
 use ark_std::marker::PhantomData;
 use ark_std::ops::Mul;
 use ark_std::rand::Rng;
+use digest::Digest;
 
-pub struct SecretKey<C: CurveGroup>(C::ScalarField);
-pub type PublicKey<C> = <C as CurveGroup>::Affine;
-
-pub struct Parameters<C: CurveGroup> {
-    adaptor_pubkey: PublicKey<C>,
-}
-
-pub struct PreSignature<C: CurveGroup>(C::Affine);
-pub struct AdaptedSignature<C: CurveGroup> {
-    pre: C::Affine,
-    adapted: C::Affine,
-}
+pub struct PreSchnorr<C: CurveGroup>(C::Affine);
+pub struct AdaptedSchnorr<C: CurveGroup>(C::Affine);
 
 pub trait AdaptorSignatureScheme {
-    type Parameters;
     type PublicKey;
     type SecretKey;
+    type PreSignature;
     type AdaptedSignature;
 
     fn keygen<R: Rng>(rng: &mut R) -> Result<(Self::PublicKey, Self::SecretKey), Error>;
 
     fn pre_sign<R: Rng>(
-        parameters: &Self::Parameters,
+        adaptor_pk: &Self::PublicKey,
         signer_sk: &Self::SecretKey,
         message: &[u8],
         rng: &mut R,
-    ) -> Result<Self, Error>
-    where
-        Self: Sized;
+    ) -> Result<Self::PreSignature, Error>;
 
     fn verify(
-        &self,
-        prameters: &Self::Parameters,
+        signature: &Self::PreSignature,
+        adaptor_pk: &Self::PublicKey,
         signer_pk: &Self::PublicKey,
         message: &[u8],
     ) -> Result<(), Error>;
 
     fn adapt(
-        self,
+        signature: Self::PreSignature,
         signer_pk: &Self::PublicKey,
         adaptor_sk: &Self::SecretKey,
     ) -> Result<Self::AdaptedSignature, Error>;
+
+    fn extract(
+        signature: &Self::AdaptedSignature,
+        pre_signature: &Self::PreSignature,
+        adaptor_pk: &Self::PublicKey,
+    ) -> Result<Self::SecretKey, Error>;
 }
 
-impl<C: CurveGroup> AdaptorSignatureScheme for PreSignature<C> {
-    type Parameters = Parameters<C>;
-    type SecretKey = SecretKey<C>;
-    type PublicKey = PublicKey<C>;
+impl<C: CurveGroup, D: Digest> AdaptorSignatureScheme for Schnorr<C, D> {
+    type SecretKey = <Self as SignatureScheme>::SecretKey;
+    type PublicKey = <Self as SignatureScheme>::PublicKey;
+    type PreSignature = PreSignature<C>;
     type AdaptedSignature = AdaptedSignature<C>;
 
-    fn keygen<R: Rng>(rng: &mut R) -> Result<(Self::PublicKey, Self::SecretKey), Error> {
-        let secret_key = C::ScalarField::rand(rng);
-        let public_key = <C::Affine as AffineRepr>::generator()
-            .mul(secret_key)
-            .into();
-        Ok((public_key, SecretKey(secret_key)))
-    }
-
     fn pre_sign<R: Rng>(
-        parameters: &Self::Parameters,
+        adaptor_pk: &Self::PublicKey,
         signer_sk: &Self::SecretKey,
         message: &[u8],
         rng: &mut R,
     ) -> Result<Self, Error> {
-        // TODO
-        Ok(PreSignature(C::rand(rng).into()))
+        // Parameter could be the adaptor public key??
+        let random_nonce = C::ScalarField::rand(rng);
+        todo!();
     }
 
     fn verify(
-        &self,
-        parameters: &Self::Parameters,
+        signature: &Self::PreSignature,
+        adaptor_pk: &Self::PublicKey,
         signer_pk: &Self::PublicKey,
         message: &[u8],
     ) -> Result<(), Error> {
@@ -85,21 +73,22 @@ impl<C: CurveGroup> AdaptorSignatureScheme for PreSignature<C> {
     }
 
     fn adapt(
-        self,
+        signature: Self::PreSignature,
         signer_pk: &Self::PublicKey,
         adaptor_sk: &Self::SecretKey,
     ) -> Result<Self::AdaptedSignature, Error> {
         // TODO
         Ok(AdaptedSignature {
-            pre: self.0,
+            pre: signature.0,
             adapted: <C::Affine as AffineRepr>::generator(),
         })
     }
-}
 
-impl<C: CurveGroup> AdaptedSignature<C> {
-    pub fn extract(&self, adaptor_pk: PublicKey<C>) -> SecretKey<C> {
-        // TODO
-        SecretKey(C::ScalarField::zero())
+    fn extract(
+        signature: &Self::AdaptedSignature,
+        pre_signature: &Self::PreSignature,
+        adaptor_pk: &Self::PublicKey,
+    ) -> Result<Self::SecretKey, Error> {
+        Ok(SecretKey(C::ScalarField::zero()))
     }
 }
