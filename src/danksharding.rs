882422df5a0218821531acbc9b14e02163293944
PopcornPaws <lipschitz77@gmail.com>
2023-09-14 17:20:14 +0200

    Wip danksharding.

--- src/danksharding.rs
+++ src/danksharding.rs
@@ -1,3 +1,11 @@
 // https://github.com/sifraitech/rust-kzg/tree/main/arkworks/src
 // https://github.com/ethereum/research/blob/master/kzg_data_availability/kzg_proofs.py
 // https://github.com/khovratovich/Kate/blob/master/Kate_amortized.pdf
+
+
+pub struct Danksharding<const N: usize, const M: usize>;
+
+pub struct Data;
+pub struct Extension;
+pub struct Proof;
+pub struct Commitment;
